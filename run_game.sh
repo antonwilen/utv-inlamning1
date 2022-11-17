f8da8f69cb269c74c28c91ef6a27d28ada16d57a
antonwilen <antwil1107@skola.goteborg.se>
2022-11-17 13:35:16 +0100

    Update run script to inform when the files has been deleted

--- run_game.sh
+++ run_game.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+echo "Anton Wilén"
+mkdir anton_wilen_labb
+cp Guesser.java anton_wilen_labb/Guesser.java
+cp GuessingGame.java anton_wilen_labb/GuessingGame.java
+cd anton_wilen_labb
+pwd
+echo "compiling..."
+javac Guesser.java
+javac GuessingGame.java
+echo "starting..."
+java GuessingGame.java
+echo "Done!"
+echo "Removing class files"
+rm Guesser.class
+rm GuessingGame.class
+ls
